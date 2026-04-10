96445b109bf0b97b69c4975911fb4ee95d926d95
Somendra singh <93323212+SomenSin@users.noreply.github.com>
2026-04-10 23:41:28 +0530

    fix: simplify deployment workflow and remove redudant prefix

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/Swastik-Dental-Care",
-  assetPrefix: "/Swastik-Dental-Care",
   images: {
     unoptimized: true,
   },
